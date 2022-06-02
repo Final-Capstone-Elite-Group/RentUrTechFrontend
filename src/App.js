9b52f234ab61586bff0c3376b397c1bdc2ffdf75
Zeeshan Haider <zhadier39@gmail.com>
2022-06-03 01:12:17 +0500

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+
 
 const App = () => (
   <div className="App">
-    Hello
+    <h1> Hello There </h1>
+    <Routes>
+        <Route index element={<h1>"home</h1>} />
+        <Route path="/login" element={<h1>login</h1>} />
+        <Route path="/signup" element={<h1>sign-up</h1>} />
+        <Route path="/details/:id" element={<h1>details</h1>} />
+        <Route path="/rent/:id" element={<h1>rent</h1>} />
+        <Route path="/add-equipment" element={<h1>add</h1>} />
+        <Route path="/delete-equipment" element={<h1>delete</h1>} />
+        <Route path="*" element={<p>There's nothing here: 404!</p>} />
+      </Routes>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 // import { Provider } from 'react-redux';
 import App from './App';
 
@@ -11,7 +12,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     {/* </Provider> */}
   </React.StrictMode>,
 );
