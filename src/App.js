238f84e333fe846e3fa0c5a0b98755aa3f9072f8
Zeeshan Haider <zhadier39@gmail.com>
2022-06-03 02:49:58 +0500

    Add protected Routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/protected-route/ProtectedRoute';
 import './App.scss';
 
-
-const App = () => (
-  <div className="App">
-    <h1> Hello There </h1>
-    <Routes>
-        <Route index element={<h1>"home</h1>} />
+const App = () => {
+  const [user] = React.useState({
+    id: '1',
+    name: 'robin',
+    roles: ['hahaha'],
+  });
+  return (
+    <div className="App">
+      <h1> Hello There </h1>
+      <Routes>
+        <Route index element={<h1>home</h1>} />
         <Route path="/login" element={<h1>login</h1>} />
         <Route path="/signup" element={<h1>sign-up</h1>} />
         <Route path="/details/:id" element={<h1>details</h1>} />
-        <Route path="/rent/:id" element={<h1>rent</h1>} />
-        <Route path="/add-equipment" element={<h1>add</h1>} />
-        <Route path="/delete-equipment" element={<h1>delete</h1>} />
-        <Route path="*" element={<p>There's nothing here: 404!</p>} />
+        <Route path="/rent" element={<ProtectedRoute element={<h1>rent</h1>} isAllowed={!!user} />} />
+        <Route path="/add-equipment" element={<ProtectedRoute element={<h1>add</h1>} isAllowed={!!user && user.roles.includes('admin')} />} />
+        <Route path="/delete-equipment" element={<ProtectedRoute element={<h1>delete</h1>} isAllowed={!!user && user.roles.includes('admin')} />} />
+        <Route path="*" element={<p>There nothing here: 404!</p>} />
       </Routes>
-  </div>
-);
+    </div>
+  );
+};
 
 export default App;
