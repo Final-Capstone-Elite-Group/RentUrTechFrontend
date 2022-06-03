076c9fd6897c603130fa6fec80a688d565c680cc
Leonardo Albornoz <albornozleo95@gmail.com>
2022-06-03 16:01:48 -0400

    Added mixins and globals

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configure_store';
 import App from './App';
-import './index.scss';
+import './sass/global.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
