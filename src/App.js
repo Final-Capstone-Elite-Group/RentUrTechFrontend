85931b274c1f9470ae5760025af0b4e8733abe3b
Zeeshan Haider <zhadier39@gmail.com>
2022-06-06 19:18:16 +0500

    Update route names a little in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProtectedRoute from './components/protected-route/ProtectedRoute';
+import Navigation from './components/navigation/Navigation';
 import './App.scss';
 
 const App = () => {
@@ -9,15 +10,16 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1> Hello There </h1>
+      <Navigation />
       <Routes>
         <Route index element={<h1>home</h1>} />
         <Route path="/login" element={<h1>login</h1>} />
         <Route path="/signup" element={<h1>sign-up</h1>} />
         <Route path="/details/:id" element={<h1>details</h1>} />
-        <Route path="/rent" element={<ProtectedRoute element={<h1>rent</h1>} isAllowed={!!user} />} />
+        <Route path="/my-reservations" element={<h1>My Reservations</h1>} />
+        <Route path="/reserve" element={<ProtectedRoute element={<h1>rent</h1>} isAllowed={!!user} />} />
         <Route path="/add-equipment" element={<ProtectedRoute element={<h1>add</h1>} isAllowed={!!user && user.roles.includes('admin')} />} />
-        <Route path="/delete-equipment" element={<ProtectedRoute element={<h1>delete</h1>} isAllowed={!!user && user.roles.includes('admin')} />} />
+        <Route path="/remove-equipment" element={<ProtectedRoute element={<h1>delete</h1>} isAllowed={!!user && user.roles.includes('admin')} />} />
         <Route path="*" element={<p>There nothing here: 404!</p>} />
       </Routes>
     </div>
