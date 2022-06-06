9798eacf93afadaf809d0891897e706c18d5adc9
Nicolaswg <nigonbol123@gmail.com>
2022-06-06 18:24:33 -0400

    Add from squeleton || Setup post request for user

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import style from './signup.module.scss';
 
 const Signup = () => {
   const [user, setUser] = useState({
@@ -7,34 +9,51 @@ const Signup = () => {
     email: '',
     password: '',
   });
+
+  const handleSubmit = () => {
+    axios.post('http://localhost:3000/signup',
+      {
+        user: {
+          name: user.name,
+          username: user.username,
+          password: user.password,
+          email: user.email,
+        },
+      }, { withCredentials: true })
+      .then((response) => {
+        console.log('registration res: ', response);
+      });
+  };
+
   return (
-    <div className="wrapper">
-      <form>
+    <div className={style.wrapper}>
+      <form onSubmit={handleSubmit()}>
         <h1>Sign up</h1>
-        <div className="form-group">
-          <label htmlFor="name">
+        <div className={style['form-group']}>
+          <span htmlFor="name">
             Name
-            <input type="text" id="name" name="name" value={user.name} onChange={(e) => setUser({ ...user, name: e.target.value })} required />
-          </label>
+          </span>
+          <input type="text" id="name" name="name" value={user.name} onChange={(e) => setUser({ ...user, name: e.target.value })} required />
         </div>
-        <div className="form-group">
-          <label htmlFor="username">
+        <div className={style['form-group']}>
+          <span htmlFor="username">
             Username
-            <input type="text" id="username" name="username" value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} required />
-          </label>
+          </span>
+          <input type="text" id="username" name="username" value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} required />
         </div>
-        <div className="form-group">
-          <label htmlFor="email">
+        <div className={style['form-group']}>
+          <span htmlFor="email">
             Email
-            <input type="email" id="email" name="email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} required />
-          </label>
+          </span>
+          <input type="email" id="email" name="email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} required />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">
+        <div className={style['form-group']}>
+          <span htmlFor="password">
             Password
-            <input type="password" id="password" name="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} required />
-          </label>
+          </span>
+          <input type="password" id="password" name="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} required />
         </div>
+        <button type="submit">Signup</button>
       </form>
     </div>
   );
