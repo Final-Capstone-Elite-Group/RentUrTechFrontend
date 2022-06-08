34b3d90d0c517d96b3f80170ae1bc0e23f0ba91b
Zeeshan Haider <zhadier39@gmail.com>
2022-06-08 17:37:43 +0500

    Set up my reservations to get data from reducer

--- src/components/my-reservations/MyReservations.js
+++ src/components/my-reservations/MyReservations.js
@@ -1,10 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getReservationsFromAPI } from '../../redux/reservation/reservation';
 
 const MyReservations = () => {
-  // const reservations = useSelector((state) => state.reservations);
+  const reservations = useSelector((state) => state.reservation);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getReservationsFromAPI());
@@ -12,7 +12,7 @@ const MyReservations = () => {
   return (
     <div>
       Hello
-      {/* {reservations.map((reserve) => <div>{reserve}</div>)} */}
+      {reservations.map((reserve) => <div key={reserve.id}>{reserve.id}</div>)}
     </div>
   );
 };
