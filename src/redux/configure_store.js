575b464cadbd5a6be2f9b60c6deed7f670552e24
Zeeshan Haider <zhadier39@gmail.com>
2022-06-08 15:25:42 +0500

    configure store to add reservationsReducer

--- src/redux/configure_store.js
+++ src/redux/configure_store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/user';
+import reservationsReducer from './reservation/reservation';
 
-const store = configureStore({ reducer: { user: userReducer } });
+const store = configureStore({ reducer: { user: userReducer, reservation: reservationsReducer } });
 export default store;
