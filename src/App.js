af5114f8051659254380e13a880dc87b0f269b51
Zeeshan Haider <zhadier39@gmail.com>
2022-06-08 17:37:21 +0500

    Add toast container inside app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useSelector } from 'react-redux';
 import ProtectedRoute from './components/protected-route/ProtectedRoute';
 import Navigation from './components/navigation/Navigation';
@@ -13,15 +15,49 @@ const App = () => {
   return (
     <div className="App">
       <Navigation />
+      <ToastContainer
+        position="top-right"
+        theme="colored"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+      />
       <Routes>
         <Route index element={<h1>home</h1>} />
         <Route path="/login" element={<h1>login</h1>} />
         <Route path="/signup" element={<h1>sign-up</h1>} />
         <Route path="/details/:id" element={<h1>details</h1>} />
-        <Route path="/my-reservations" element={<ProtectedRoute element={<MyReservations />} isAllowed={!!user} />} />
-        <Route path="/reserve" element={<ProtectedRoute element={<Reserve />} isAllowed={!!user} />} />
-        <Route path="/add-equipment" element={<ProtectedRoute element={<h1>add</h1>} isAllowed={!!user && user.roles.includes('admin')} />} />
-        <Route path="/remove-equipment" element={<ProtectedRoute element={<h1>delete</h1>} isAllowed={!!user && user.roles.includes('admin')} />} />
+        <Route
+          path="/my-reservations"
+          element={<ProtectedRoute element={<MyReservations />} isAllowed={!!user} />}
+        />
+        <Route
+          path="/reserve"
+          element={<ProtectedRoute element={<Reserve />} isAllowed={!!user} />}
+        />
+        <Route
+          path="/add-equipment"
+          element={(
+            <ProtectedRoute
+              element={<h1>add</h1>}
+              isAllowed={!!user && user.roles.includes('admin')}
+            />
+    )}
+        />
+        <Route
+          path="/remove-equipment"
+          element={(
+            <ProtectedRoute
+              element={<h1>delete</h1>}
+              isAllowed={!!user && user.roles.includes('admin')}
+            />
+    )}
+        />
         <Route path="*" element={<p>There nothing here: 404!</p>} />
       </Routes>
     </div>
