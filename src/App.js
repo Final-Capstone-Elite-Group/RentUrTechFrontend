988bb3f22334d9207725c4c29ee278bf66668ddb
Nicolaswg <nigonbol123@gmail.com>
2022-06-09 18:09:32 -0400

    Enable add equipment route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from './components/login/Login';
 import './App.scss';
 import Carousel from './components/carousel/Carousel';
 import initState from './redux/equipment/equipmentAPI';
+import AddEquipment from './components/equipment/AddEquipment';
 
 const App = () => {
   const user = useSelector((state) => state.auth);
@@ -18,7 +19,7 @@ const App = () => {
   useEffect(() => {
     dispatch(initState());
   }, []);
-  console.log(equipments);
+  console.log(user);
 
   return (
     <div className="App">
@@ -27,12 +28,11 @@ const App = () => {
         <Route index element={<Carousel collection={equipments} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* <Route path="/signup" element={<ProtectedRoute element={<Signup />} isAllowed={!user} redirectPath="/" />} /> */}
         <Route path="/details/:id" element={<h1>details</h1>} />
         <Route path="/my-reservations" element={<h1>My Reservations</h1>} />
         <Route path="/reserve" element={<ProtectedRoute element={<h1>rent</h1>} isAllowed={!!user} />} />
-        {/* <Route path="/add-equipment" element={<ProtectedRoute element={<h1>add</h1>} isAllowed={!!user && user.role.includes('admin')} />} />
-        <Route path="/remove-equipment" element={<ProtectedRoute element={<h1>delete</h1>} isAllowed={!!user && user.role.includes('admin')} />} /> */}
+        <Route path="/add-equipment" element={<AddEquipment />} />
+        {/* <Route path="/remove-equipment" element={<ProtectedRoute element={<h1>delete</h1>} isAllowed={!!user && user.role.includes('admin')} />} /> */}
         <Route path="*" element={<p>There nothing here: 404!</p>} />
       </Routes>
     </div>
