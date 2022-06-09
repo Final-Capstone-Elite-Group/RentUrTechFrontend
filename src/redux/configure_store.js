a40bb6e9817aee16588b98ab57d8824f97c3f98c
Nicolas Gonzalez <Nigonbol123@gmail.com>
2022-06-09 16:16:37 -0400

    Fix linters

--- src/redux/configure_store.js
+++ src/redux/configure_store.js
@@ -3,10 +3,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './user/user';
 import equipmentReducer from './equipment/equipment';
 
-const store = configureStore({ reducer: { auth: userReducer, equipment: equipmentReducer } });
-
-
-
-
+const store = configureStore({ reducer: { auth: authReducer, equipment: equipmentReducer } });
 
 export default store;
