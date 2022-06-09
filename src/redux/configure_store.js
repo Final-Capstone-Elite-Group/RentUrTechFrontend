a3eb12138898cdfa87da729709fe32e3a7012a66
Leonardo Albornoz <albornozleo95@gmail.com>
2022-06-09 14:23:15 -0400

    Add reducer to config

--- src/redux/configure_store.js
+++ src/redux/configure_store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/user';
+import equipmentReducer from './equipment/equipment';
 
-const store = configureStore({ reducer: { user: userReducer } });
+const store = configureStore({ reducer: { user: userReducer, equipment: equipmentReducer } });
 export default store;
