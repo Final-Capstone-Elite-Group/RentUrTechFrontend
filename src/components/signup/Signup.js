b7f2a75d7d6fbebe87f8e8b593986ec5c6d99ae9
Nicolaswg <nigonbol123@gmail.com>
2022-06-10 07:41:38 -0400

    Modify scss form class

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -31,7 +31,7 @@ const Signup = () => {
     <>
       <ToastContainer />
       <div className={style.wrapper}>
-        <form onSubmit={handleSubmit} className={style['user-form']}>
+        <form onSubmit={handleSubmit} className={style.form}>
           <div>
             <h1>Sign up</h1>
             <div className={style.logo} />
