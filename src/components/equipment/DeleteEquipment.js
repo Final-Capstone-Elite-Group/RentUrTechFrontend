471f2dec626f05c370e5b26b2019a4aa374d8631
Nicolaswg <nigonbol123@gmail.com>
2022-06-13 15:31:39 -0400

    Create delete equipment component

--- src/components/equipment/DeleteEquipment.js
+++ src/components/equipment/DeleteEquipment.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import DataTable from 'react-data-table-component';
 import style from './delete_equipment.module.scss';
 
 const DeleteEquipment = () => {
   const { equipments } = useSelector((state) => state.equipment);
+  const columns = [
+    {
+      name: 'id',
+      selector: (row) => row.id,
+    },
+    {
+      name: 'Title',
+      selector: (row) => row.title,
+    },
+    {
+      name: 'Action',
+      cell: () => <button type="button" className={style['btn-delete']}>Delete</button>,
+    },
+  ];
+  console.log(equipments);
   return (
-    <ul className={style.wrapper}>
-      {equipments.map((item) => (
-        <li className={style.card} key={item.id}>
-          <span>{item.title}</span>
-        </li>
-      ))}
-    </ul>
+    <div className={style.wrapper}>
+      <DataTable columns={columns} data={equipments} fixedHeader pagination />
+    </div>
   );
 };
 
