e75c1c2f1ad893b799eee213acac449569f9be70
Nicolaswg <nigonbol123@gmail.com>
2022-06-13 17:05:57 -0400

    Call API delete equipment request

--- src/components/equipment/DeleteEquipment.js
+++ src/components/equipment/DeleteEquipment.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import DataTable from 'react-data-table-component';
 import style from './delete_equipment.module.scss';
+import { destroyEquipmentFromAPI } from '../../redux/equipment/equipmentAPI';
 
 const DeleteEquipment = () => {
   const { equipments } = useSelector((state) => state.equipment);
+  const dispatch = useDispatch();
   const columns = [
     {
       name: 'id',
       selector: (row) => row.id,
+      sortable: true,
     },
     {
       name: 'Title',
@@ -16,10 +19,21 @@ const DeleteEquipment = () => {
     },
     {
       name: 'Action',
-      cell: () => <button type="button" className={style['btn-delete']}>Delete</button>,
+      cell: (row) => (
+        <button
+          type="button"
+          className={style['btn-delete']}
+          onClick={((e) => {
+            e.preventDefault();
+            dispatch(destroyEquipmentFromAPI(row.id));
+          })}
+        >
+          Delete
+        </button>
+      ),
     },
   ];
-  console.log(equipments);
+
   return (
     <div className={style.wrapper}>
       <DataTable columns={columns} data={equipments} fixedHeader pagination />
