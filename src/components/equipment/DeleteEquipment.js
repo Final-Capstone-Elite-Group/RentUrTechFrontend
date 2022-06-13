c700ffffd6e47a994b9ab46455c4728503b9c339
Nicolaswg <nigonbol123@gmail.com>
2022-06-13 18:08:01 -0400

    Implement reload request when deleting a equipment

--- src/components/equipment/DeleteEquipment.js
+++ src/components/equipment/DeleteEquipment.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import DataTable from 'react-data-table-component';
 import style from './delete_equipment.module.scss';
+import { equipmentsQuery } from '../../logic/queries';
 import { destroyEquipmentFromAPI } from '../../redux/equipment/equipmentAPI';
 
 const DeleteEquipment = () => {
   const { equipments } = useSelector((state) => state.equipment);
-  const dispatch = useDispatch();
+  const { isLoading, refetch } = equipmentsQuery();
   const columns = [
     {
       name: 'id',
@@ -25,7 +26,7 @@ const DeleteEquipment = () => {
           className={style['btn-delete']}
           onClick={((e) => {
             e.preventDefault();
-            dispatch(destroyEquipmentFromAPI(row.id));
+            destroyEquipmentFromAPI(row.id, refetch);
           })}
         >
           Delete
@@ -33,6 +34,10 @@ const DeleteEquipment = () => {
       ),
     },
   ];
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
 
   return (
     <div className={style.wrapper}>
