19fb0a1387f42184229c69dd65904fa18ab78ef3
Leonardo Albornoz <albornozleo95@gmail.com>
2022-06-14 16:15:44 -0400

    Add money formatter

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,18 +1,75 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { useParams, Navigate, Link } from 'react-router-dom';
 import { currentEquipment } from '../../redux/equipment/equipment';
+import Button from '../button/Button';
+import ColorPallete from '../color-pallete/ColorPallete'
+import { BiLeftArrow } from 'react-icons/bi';
+import { GrCycle } from 'react-icons/gr';
+import { FaChevronRight } from 'react-icons/fa';
+import styles from './Details.module.scss';
 
 const Details = () => {
-  const { currentTech } = useSelector(state => state.equipment);
+  const { 
+    detailsContainer,
+    imageContainer,
+    flex,
+    details,
+    discover,
+    chevron
+  } = styles;
+  const { currentTech, equipments } = useSelector(state => state.equipment);
   const dispatch = useDispatch();
+  const { id } = useParams();
 
   useEffect(() => {
-    dispatch(currentEquipment(1));
-    console.log(currentTech)
+    dispatch(currentEquipment(id.toString()));
   }, []);
 
+  var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+
+    // These options are needed to round to whole numbers if that's what you want.
+    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  });
+
   return (
-    <h1>hello</h1>
+    currentTech ? 
+    <div className={detailsContainer}>
+      <Link to="/">
+        <Button style={{display: 'flex', position: 'absolute', bottom: 0, left: 0}}><BiLeftArrow /></Button>
+      </Link>
+      <div className={imageContainer}>
+        <figure>
+          <img src={currentTech.attributes.image.url} alt={currentTech.attributes.title} />
+        </figure>
+        <span><GrCycle /></span>
+        <h6>Rotate<br />vehicle</h6>
+      </div>
+      <div className={`${flex} ${details}`}>
+        <h2>{currentTech.attributes.title}</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td><h5>Rent fee</h5>
+              {formatter.format(currentTech.attributes.rent_fee)}</td>
+            </tr>
+            <tr>
+              <td><h5>Total amount payable</h5>
+              {formatter.format(currentTech.attributes.total_amount_payable)}</td>
+            </tr>
+            <tr>
+              <td><h5>Duration</h5>
+              {formatter.format(currentTech.attributes.duration)}</td>
+            </tr>
+          </tbody>
+        </table> 
+        <Link className={discover} to="#">DISCOVER MORE MODELS <span className={chevron}><FaChevronRight /></span></Link>
+        <ColorPallete />
+      </div>
+    </div> : equipments.length > 0 ? "loading" : <Navigate push to={'/'}/>
   );
 }
 
