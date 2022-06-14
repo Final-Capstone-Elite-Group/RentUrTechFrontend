5b88bc613bb0582bec8ff519853cb4c80a8d5626
Leonardo Albornoz <albornozleo95@gmail.com>
2022-06-14 02:45:54 -0400

    Add color pallete

---
+++ src/components/color-pallete/ColorPallete.js
@@ -0,0 +1,77 @@
+import { useEffect, useRef } from 'react';
+
+const ColorPallete = () => {
+  const canvas = useRef(null);
+  function byte2Hex(n) {
+    let nybHexString = "0123456789ABCDEF";
+    return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
+  }
+
+  function RGB2Color(r,g,b) {
+    return '#' + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
+  }
+
+  function getColor(item, maxitem) {
+    let phase = 0;
+    let center = 128;
+    let width = 127;
+    let frequency = Math.PI*2/maxitem;
+    
+    const red   = Math.sin(frequency*item+2+phase) * width + center;
+    const green = Math.sin(frequency*item+0+phase) * width + center;
+    const blue  = Math.sin(frequency*item+4+phase) * width + center;
+    
+    return RGB2Color(red,green,blue);
+  }
+
+  const drawRouletteWheel = () => {
+    if (canvas.current.getContext) {
+      const w = canvas.current.width ;
+      let outsideRadius = w / 2.5;
+      let textRadius = w / 3.125;
+      let insideRadius = w / 4;
+      let arc = Math.PI / 9;
+
+      const ctx = canvas.current.getContext("2d");
+      ctx.clearRect(0,0,w,w);
+
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+
+      ctx.font = 'bold 12px Helvetica, Arial';
+
+      for(let i = 0; i < 18; i++) {
+        let angle = 0 + i * arc;
+        //ctx.fillStyle = colors[i];
+        ctx.fillStyle = getColor(i, 18);
+
+        ctx.beginPath();
+        ctx.arc(w / 2, w / 2, outsideRadius, angle, angle + arc, false);
+        ctx.arc(w / 2, w / 2, insideRadius, angle + arc, angle, true);
+        ctx.stroke();
+        ctx.fill();
+
+        ctx.save();
+        ctx.shadowOffsetX = -1;
+        ctx.shadowOffsetY = -1;
+        ctx.shadowBlur    = 0;
+        ctx.shadowColor   = "rgb(220,220,220)";
+        ctx.fillStyle = "black";
+        ctx.translate(w / 2 + Math.cos(angle + arc / 2) * textRadius, 
+                      w / 2 + Math.sin(angle + arc / 2) * textRadius);
+        ctx.rotate(angle + arc / 2 + Math.PI / 2);
+        ctx.restore();
+      } 
+    }
+  }
+  useEffect(() => {
+    drawRouletteWheel();
+  }, [])
+  return (
+    <canvas  ref={canvas} width='150' height='150' />
+  );
+}
+
+
+export default ColorPallete;
+
