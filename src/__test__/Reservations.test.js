3fc8c4e279ffa25cbd100b89cefe86d6645d328b
Nicolaswg <nigonbol123@gmail.com>
2022-06-14 13:35:46 -0400

    Modify reservation test cases

--- src/__test__/Reservations.test.js
+++ src/__test__/Reservations.test.js
@@ -26,16 +26,6 @@ jest.mock('react-select', () => ({ options = [{ label: 'tech', value: 1 }], valu
   );
 });
 
-jest.mock('../logic/apiRequests', () => {
-  const originalModule = jest.requireActual('../logic/apiRequests');
-  // Mock the default export and named export 'createReservation'
-  return {
-    __esModule: true,
-    ...originalModule,
-    createReservation: jest.fn(() => true),
-  };
-});
-
 afterEach(() => cleanup(), server.resetHandlers());
 
 beforeAll(() => server.listen({
@@ -59,6 +49,72 @@ beforeEach(() => {
         role: 'admin',
       },
       token: '123',
+    },
+    reservation:
+       [
+         {
+           id: 15,
+           total: '52.0',
+           reserved_date: '2022-07-26',
+           city: 'Somewhere nice',
+           created_at: '2022-06-09T15:07:15.493Z',
+           updated_at: '2022-06-09T15:07:15.493Z',
+           equipment: {
+             id: 14,
+             title: 'Wireless Printer',
+             url: 'http://localhost:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBGdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--9d122d8fb7af2f04562aa6a26d728ed7d3fe8c64/drone1.jpg',
+             duration: 5,
+           },
+         },
+       ],
+    equipment: {
+      equipments: [
+        {
+          id: 14,
+          title: 'Wireless Printer',
+          description: "The Wireless Printer gives you all the essential features you need—easily print, scan and copy, and activate HP+: HP's smart printing system that comes with 6 months of free Instant Ink and a 2 year extended HP warranty.",
+          review: 'https://www.youtube.com/watch?v=9Xk39LNg304',
+          duration: 5,
+          rent_fee: '821.0',
+          dates_reserved: [],
+          total_amount_payable: '22.0',
+          created_at: '2022-06-08T19:42:08.103Z',
+          updated_at: '2022-06-08T19:42:08.112Z',
+          image: {
+            url: 'http://localhost:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBJUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2c533787f3499541cdd2bd6da005bace4749ddb7/wireless_printer.jpeg',
+          },
+        },
+        {
+          id: 13,
+          title: 'Turntable',
+          description: 'Whether you\'re just getting into records, rediscovering a record collection long since buried away or simply adding a turntable to complete your system, the AT-LP60X fully automatic belt-drive turntable is an excellent choice. This affordable, easy-to-use turntable plays both 33-1/3 and 45 RPM records and, with its built-in switchable phonon preamp, can be connected directly to your home stereo, powered speakers, computer and other components, whether they have a dedicated phonon input or not. An update of the popular AT-LP60, the ATLP60X features a redesigned tone arm base and head shell to improve tracking and reduce resonance. Noise is further reduced by the inclusion of an AC adapter that moves the AC/DC conVersion outside of the chassis where its impact on the signal chain is limited. Audio-Technical has been a leader in phonon cartridge design for more than 50 years, and that expertise shows in the AT-LP60X\'s Dual Magnet cartridge with replaceable stylus. The cartridge is integrated into the head shell for assured performance and ease of setup. The turntable comes with a detachable RCA output cable (3. 5 mm male to dual RCA male), 45 RPM adapter, and a removable hinged dust cover. It is available in black (AT-LP60X-BK), brown/black (AT-LP60X-BW), gunmetal/black (AT-LP60X-GM), and red/black (AT-LP60X-RD).',
+          review: 'https://www.youtube.com/watch?v=Yaj1Mhj8mo4',
+          duration: 6,
+          rent_fee: '832.0',
+          dates_reserved: [],
+          total_amount_payable: '26.0',
+          created_at: '2022-06-08T19:42:08.067Z',
+          updated_at: '2022-06-08T19:42:08.081Z',
+          image: {
+            url: 'http://localhost:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBJQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ee3004a8fe9fce926a806f65e4a87b9aa25c45af/turntable.jpeg',
+          },
+        }],
+      currentTech: {
+        id: 13,
+        title: 'Turntable',
+        description: 'Whether you\'re just getting into records, rediscovering a record collection long since buried away or simply adding a turntable to complete your system, the AT-LP60X fully automatic belt-drive turntable is an excellent choice. This affordable, easy-to-use turntable plays both 33-1/3 and 45 RPM records and, with its built-in switchable phonon preamp, can be connected directly to your home stereo, powered speakers, computer and other components, whether they have a dedicated phonon input or not. An update of the popular AT-LP60, the ATLP60X features a redesigned tone arm base and head shell to improve tracking and reduce resonance. Noise is further reduced by the inclusion of an AC adapter that moves the AC/DC conVersion outside of the chassis where its impact on the signal chain is limited. Audio-Technical has been a leader in phonon cartridge design for more than 50 years, and that expertise shows in the AT-LP60X\'s Dual Magnet cartridge with replaceable stylus. The cartridge is integrated into the head shell for assured performance and ease of setup. The turntable comes with a detachable RCA output cable (3. 5 mm male to dual RCA male), 45 RPM adapter, and a removable hinged dust cover. It is available in black (AT-LP60X-BK), brown/black (AT-LP60X-BW), gunmetal/black (AT-LP60X-GM), and red/black (AT-LP60X-RD).',
+        review: 'https://www.youtube.com/watch?v=Yaj1Mhj8mo4',
+        duration: 6,
+        rent_fee: '832.0',
+        dates_reserved: ['2022-12-12'],
+        total_amount_payable: '26.0',
+        created_at: '2022-06-08T19:42:08.067Z',
+        updated_at: '2022-06-08T19:42:08.081Z',
+        image: {
+          url: 'http://localhost:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBJQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ee3004a8fe9fce926a806f65e4a87b9aa25c45af/turntable.jpeg',
+        },
+
+      },
     },
   };
 });
@@ -110,8 +166,8 @@ it('Clicking on My Reservations takes you to the your Reservations page', async 
 
 it('Clicking on Trashcan deletes the Reservation', async () => {
   render(<App />, { preloadedState });
-  await waitFor(() => screen.getByText('GPS Drone'));
+  await waitFor(() => screen.getByText('Wireless Printer'));
   fireEvent.click(screen.getByTestId('trashcan'));
-  await waitFor(() => screen.getByText('Reservations updated successfully'));
-  expect(screen.getByText('Reservations updated successfully')).toBeInTheDocument();
+  screen.getByText('Wireless Printer');
+  await waitFor(() => expect(screen.queryByText('Wireless Printer')).not.toBeInTheDocument());
 });
